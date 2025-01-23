7d43ad4383ddf9309fce0d1e79825cf6fa3d46a2
SOT <devprod99@gmail.com>
2025-01-23 19:27:54 +0530

    7:27

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller("/")
+export class AppController{
+    @Get()
+    getInfo()
+    {
+        return {
+            "data":"abc",
+            "message":"Success",
+            "status":200
+        };
+    }
+}
