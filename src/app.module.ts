72929ee96b36cf01c9e9abc5491fb311aeb0f861
Durga Prasad Upadhaya <durga.prasad@aqilea.com>
2025-01-30 09:54:24 +0530

    Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,9 @@ import {
 import { EmployeeModule } from './employee/employee.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './database/postgres.config';
-import { Middleware2 } from './common/middleware/middleware2';
 import { Middleware1 } from './common/middleware/middleware1';
-import { AuthenticationGuard } from './common/guard/authentication.guard';
 import { JwtModule } from '@nestjs/jwt';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -19,15 +18,19 @@ import { JwtModule } from '@nestjs/jwt';
       secret: '123456',
       signOptions: { expiresIn: '600s' },
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 2,
+      },
+    ]),
     TypeOrmModule.forRoot(databaseConfig),
     EmployeeModule,
   ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-    .apply(Middleware1)
-    .forRoutes("*")
+    consumer.apply(Middleware1).forRoutes('*');
     // .apply(Middleware2)
     // .forRoutes({path:'employee',method: RequestMethod.GET})
   }
--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -3,10 +3,15 @@ import { EmployeeController } from './employee.controller';
 import { EmployeeService } from './employee.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Employee } from './entity/employee.entity';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Employee])],
   controllers: [EmployeeController],
-  providers: [EmployeeService],
+  providers: [EmployeeService, {
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
 })
 export class EmployeeModule {}
