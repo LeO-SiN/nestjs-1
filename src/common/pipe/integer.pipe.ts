b9c53fdc9712f39c86c8ec61c0d07598f8f78f31
SOT <devprod99@gmail.com>
2025-01-30 20:24:21 +0530

    30010824

--- src/common/pipe/integer.pipe.ts
+++ src/common/pipe/integer.pipe.ts
@@ -1,8 +1,14 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from "@nestjs/common";
 
 @Injectable()
 export class IntegerPipe implements PipeTransform{
     transform(value: any) {
-        return Number(value)
+        const num=Number(value);
+        if(isNaN(num))
+        throw new BadRequestException({
+            "message": "Validation failed (numeric string is expected)",
+            "error": "Bad Request",
+            "statusCode": 400
+        })
     }
 }
