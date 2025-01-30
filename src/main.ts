6d16d89a57dda5867c39683052404ac0a88dabb5
SOT <devprod99@gmail.com>
2025-01-30 19:51:41 +0530

    30011751

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/interceptor/loggin.interceptor';
 
 async function start() {
   const app = await NestFactory.create(AppModule);
-  //app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalInterceptors(new LoggingInterceptor)
   await app.listen(3000);
 }
 
